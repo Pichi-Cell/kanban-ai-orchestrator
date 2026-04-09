c65d83dbc79544fc67845e103ef2e20447ca9694
Pichi-Cell <Picchilucas7@gmail.com>
2026-04-08 22:28:36 -0300

    vite .env importing fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    define: {
+      // Explicitly bridge the Docker OS variable into Vite
+      'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+    }
+  }
 })
